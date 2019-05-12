77d24e2cae8308d79ddd9e1d0c25433c8a3a9415
Ana Riat <t.riat24@gmail.com>
2019-05-12 11:58:24 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,1 @@
-def my_select(collection)
-new_collection =[]
-i = 0 
-while i < collection.length 
-item = yield collection [i] 
-if item == true 
-  modified.push(collection[i])
-end
-i += 1
-end
+
