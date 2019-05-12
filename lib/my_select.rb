409872b02c72692ddaae49684d4b406eb38c9cf8
Ana Riat <t.riat24@gmail.com>
2019-05-12 11:58:45 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,1 +1,12 @@
+def 
 
+  new_collection = []
+  while i < collection.length
+    item = yield(collection[i])
+    if item == true
+      new_collection << collection[i]
+    end
+    i = i + 1
+  end
+  new_collection
+end	end
